0a8ca08a691a05b1d140b42826f5fb19fc1fc25d
tratteo <bltmtt@gmail.com>
2023-11-17 21:10:34 +0100

    Update utils.dart

--- lib/src/activity/utils.dart
+++ lib/src/activity/utils.dart
@@ -1,17 +1,25 @@
 import "package:latlong2/latlong.dart";
 import "package:omnimodel/omnimodel.dart";
 
-List<(OmniModel activity, double distance)> getActivitiesInRange({
+List<(OmniModel activity, double distance)> getCloseActivities({
   required OmniModel activity,
+  required List<OmniModel> activities,
+  required double rangeInKm,
+}) {
+  var latitude = activity.tokenOrNull<num>("attestation.latitude")?.toDouble();
+  var longitude = activity.tokenOrNull<num>("attestation.longitude")?.toDouble();
+  if (latitude == null || longitude == null) return [];
+  var point = LatLng(latitude, longitude);
+  return getActivitiesInRange(point: point, activities: activities, rangeInKm: rangeInKm);
+}
+
+List<(OmniModel activity, double distance)> getActivitiesInRange({
+  required LatLng point,
   required List<OmniModel> activities,
   required double rangeInKm,
 }) {
   final Distance distance = Distance();
   List<(OmniModel activity, double distance)> result = List.empty(growable: true);
-  var latitude = activity.tokenOrNull<num>("attestation.latitude")?.toDouble();
-  var longitude = activity.tokenOrNull<num>("attestation.longitude")?.toDouble();
-  if (latitude == null || longitude == null) return result;
-  var point = LatLng(latitude, longitude);
   for (final model in activities) {
     var long = model.tokenOrNull<num>("attestation.longitude")?.toDouble();
     var lat = model.tokenOrNull<num>("attestation.latitude")?.toDouble();
