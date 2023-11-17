c00efd8746ea23ee658fe2acd8a7d38ebd82f638
tratteo <bltmtt@gmail.com>
2023-11-17 21:08:01 +0100

    Close activities

---
+++ lib/src/activity/utils.dart
@@ -0,0 +1,25 @@
+import "package:latlong2/latlong.dart";
+import "package:omnimodel/omnimodel.dart";
+
+List<(OmniModel activity, double distance)> getActivitiesInRange({
+  required OmniModel activity,
+  required List<OmniModel> activities,
+  required double rangeInKm,
+}) {
+  final Distance distance = Distance();
+  List<(OmniModel activity, double distance)> result = List.empty(growable: true);
+  var latitude = activity.tokenOrNull<num>("attestation.latitude")?.toDouble();
+  var longitude = activity.tokenOrNull<num>("attestation.longitude")?.toDouble();
+  if (latitude == null || longitude == null) return result;
+  var point = LatLng(latitude, longitude);
+  for (final model in activities) {
+    var long = model.tokenOrNull<num>("attestation.longitude")?.toDouble();
+    var lat = model.tokenOrNull<num>("attestation.latitude")?.toDouble();
+    if (lat == null || long == null) continue;
+    double dist = distance(point, LatLng(lat, long)) / 1000.0;
+    if (dist < rangeInKm) {
+      result.add((model, dist));
+    }
+  }
+  return result;
+}
